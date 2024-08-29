d79030c0a84502a83b856b5008816b274e99cab1
Stefany Rodrigues <132313469+stef4ny@users.noreply.github.com>
2024-08-29 14:41:10 -0300

    Add files via upload

---
+++ DB/create_tables.sql
@@ -0,0 +1,114 @@
+-- Criação da tabela de usuários
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(255) NOT NULL,
+  data_nascimento DATE NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  telefone VARCHAR(20) NOT NULL,
+  cep VARCHAR(10) NOT NULL,
+  senha VARCHAR(255) NOT NULL,
+  numero VARCHAR(10) NOT NULL,
+  createdAt DATETIME NOT NULL
+);
+
+-- Criação da tabela de pedidos
+CREATE TABLE IF NOT EXISTS orders (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  data_pedido DATETIME NOT NULL,
+  order_user_id INT NOT NULL,
+  FOREIGN KEY (order_user_id) REFERENCES users(id)
+);
+
+-- Criação da tabela de categorias
+CREATE TABLE IF NOT EXISTS category (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(45) NOT NULL,
+  descricao VARCHAR(45) NOT NULL
+);
+
+-- Criação da tabela de produtos
+CREATE TABLE IF NOT EXISTS products (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(45) NULL,
+  descricao VARCHAR(45) NOT NULL,
+  estoque INT NOT NULL,
+  data_fabricacao DATE NOT NULL,
+  valor FLOAT NOT NULL,
+  cat_id INT NOT NULL,
+  status  gENUM ('Novo', 'UltimAs unidades', 'Promoção'),
+  image_type ENUM('cover', 'other') NOT NULL, 
+  FOREIGN KEY (cat_id) REFERENCES category(id)
+);
+
+-- Criação da tabela de produtos em pedidos
+CREATE TABLE IF NOT EXISTS requests_products (
+  pedido_id INT,
+  produto_id INT,
+  quantidade INT NOT NULL,
+  PRIMARY KEY (pedido_id, produto_id),
+  FOREIGN KEY (pedido_id) REFERENCES orders(id),
+  FOREIGN KEY (produto_id) REFERENCES products(id)
+);
+
+-- Criação da tabela de recuperação de senhas
+CREATE TABLE IF NOT EXISTS password_resets (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT NOT NULL,
+  token VARCHAR(255) NOT NULL,
+  expires_at DATETIME NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- Criação da tabela de tokens
+CREATE TABLE IF NOT EXISTS tokens (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT NOT NULL,
+  token VARCHAR(255) NOT NULL,
+  expires_at DATETIME NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+--Criação da tabela favorites
+CREATE TABLE IF NOT EXISTS favorites(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT NOT NULL,
+  product_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (product_id) REFERENCES products(id),
+  UNIQUE (user_id, product_id)
+);
+
+--Criação da tabela reviews
+CREATE TABLE IF NOT EXISTS reviews(
+  id INT AUTO_INCREMENT NOT NULL,
+  product_id INT NOT NULL,
+  user_id INT NOT NULL,
+  rating INT NOT NULL CHECK(rating>=1 AND  rating<=5),
+  create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (product_id) REFERENCES products(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
+-- Insere um novo usuário apenas se a tabela de usuários estiver vazia
+INSERT INTO users (
+  nome, 
+  data_nascimento, 
+  email, 
+  senha, 
+  telefone, 
+  createdAt, 
+  numero, 
+  cep
+)
+SELECT 
+  'Administrador',
+  '2024-08-01',
+  'marvieifpe@gmail.com',
+  '$2b$10$Qckp/U5w8ICkPJ0EBQVMb.Qs2OY9W9QOn0oXBzpb.2tUr6h69KgVq',
+  '+00 00 00000-0000',
+  '2024-08-12 00:00:00',
+  '0',
+  '00000-000'
+FROM DUAL
+WHERE NOT EXISTS (SELECT 1 FROM users);
---
+++ marvie_backend/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.NODEJS_PORT || 3000;
+
+const usersRoutes = require("./routes/usersRoutes");
+const productsRoutes = require("./routes/productsRoutes");
+const ordersRoutes = require("./routes/ordersRoutes");
+const passwordResetRoutes = require("./routes/passwordResetRoutes");
+const connectRoutes = require("./routes/connectRoutes");
+const tokenRoutes = require("./routes/tokenRoutes");
+
+app.use(bodyParser.json());
+app.use(cors({ origin: "*" }));
+
+app.use("/users", usersRoutes);
+app.use("/products", productsRoutes);
+app.use("/orders", ordersRoutes);
+app.use("/password_reset", passwordResetRoutes);
+app.use("/connected", connectRoutes);
+app.use("/token", tokenRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
